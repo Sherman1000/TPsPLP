3045d9f6b94eefce8ba1213239dece517a756ede
GermanDeSousa <gdesousa@eryxsoluciones.com.ar>
2016-06-04 19:17:10 -0300

    ejercicio 10 terminado y arreglo de formato

--- Logico/esqueleto-diccionarios/esqueleto.pl
+++ Logico/esqueleto-diccionarios/esqueleto.pl
@@ -21,80 +21,134 @@ read_file(Stream,[X|L]) :-
 
 
 %Ejercicio 1
-diccionario_lista(Lcode) :- diccionario(PalabraDelDicc), string_codes(PalabraDelDicc, Lcode). 
+diccionario_lista(Lcode) :- diccionario(PalabraDelDicc), 
+							string_codes(PalabraDelDicc, Lcode). 
 
 %Ejercicio 2
 juntar_con([L], _ , L).
-juntar_con([Ls | Lss], J, R) :- juntar_con(Lss, J, Rrec), append(Rpref, Rrec, R), append(Ls, [J], Rpref),!.  
+juntar_con([Ls | Lss], J, R) :- juntar_con(Lss, J, Rrec), 
+								append(Rpref, Rrec, R), 
+								append(Ls, [J], Rpref),!.  
 
-separar_con([Ls | Lss], J, R) :- append(Rpref, Rrec, R), juntar_con(Lss, J, Rrec), append(Ls, [J], Rpref),!. 
+separar_con([Ls | Lss], J, R) :- append(Rpref, Rrec, R), 
+								 juntar_con(Lss, J, Rrec), 
+								 append(Ls, [J], Rpref),!. 
 separar_con([L], _, L).
 
 %Ejercicio 3
 palabras(S, P) :- separar_con(P, espacio, S).
 
 %Ejercicio 4
-%Preguntar sobre la respuesta que hay que dar en el codigo? Que nos estamos perdiendo? el tercer caso del TP termina sin dar false.
-asignar_var(A, MI, MF) :- getKeys(MI, Keys), not(member(A, Keys)), append(MI, [(A, X)], MF).
-asignar_var(A, MI, MI) :- getKeys(MI, Keys), member(A, Keys).
+asignar_var(A, MI, MF) :- getKeys(MI, Keys), 
+						  not(member(A, Keys)), 
+						  append(MI, [(A, X)], MF).
+asignar_var(A, MI, MI) :- getKeys(MI, Keys), 
+						  member(A, Keys).
 
 getKeys([], []).
-getKeys([(Key, Value) | Ts], Tmapped) :- getKeys(Ts, Trec), append([Key], Trec, Tmapped).
+getKeys([(Key, Value) | Ts], Tmapped) :- getKeys(Ts, Trec), 
+										 append([Key], Trec, Tmapped).
 
 %Ejercicio 5
 palabras_con_variables(L, V) :- palabras_con_var_y_mapa(L, V, [], Mf).
 
 palabras_con_var_y_mapa([], [], Mf, Mf).
-palabras_con_var_y_mapa([Ls | Lss], [Vs | Vss], M, Mf1) :- pares_definidos_en_mapa(Ls, Vs, M, Mf0), palabras_con_var_y_mapa(Lss, Vss, Mf0, Mf1).
+palabras_con_var_y_mapa([Ls | Lss], [Vs | Vss], M, Mf1) :- pares_definidos_en_mapa(Ls, Vs, M, Mf0), 
+														   palabras_con_var_y_mapa(Lss, Vss, Mf0, Mf1).
 
 pares_definidos_en_mapa([], [], Mf, Mf).
-pares_definidos_en_mapa([X | Ls], [V | Vs], Mi0, Mf) :- asignar_var(X, Mi0 , Mi1), member((X,V), Mi1), pares_definidos_en_mapa(Ls, Vs, Mi1, Mf).
+pares_definidos_en_mapa([X | Ls], [V | Vs], Mi0, Mf) :- asignar_var(X, Mi0 , Mi1), 
+														member((X,V), Mi1), 
+														pares_definidos_en_mapa(Ls, Vs, Mi1, Mf).
 
 %Ejercicio 6
 quitar(_, [], R) :- R = [].
 quitar(E, [L|Ls], R) :- E == L, quitar(E, Ls, R).
-quitar(E, [L|Ls], R) :- E \== L, quitar(E, Ls, Rrec), append([L], Rrec, R).
+quitar(E, [L|Ls], R) :- E \== L, quitar(E, Ls, Rrec), 
+						append([L], Rrec, R).
 
 %Ejercicio 7
 cant_distintos([], S) :- S = 0.
-cant_distintos([L | Ls], S) :- quitar(L, Ls, Rquitado), cant_distintos(Rquitado, CuentaRec), S is (1 + CuentaRec).
+cant_distintos([L | Ls], S) :- quitar(L, Ls, Rquitado), 
+							   cant_distintos(Rquitado, CuentaRec), 
+							   S is (1 + CuentaRec).
 
 %Ejercicio 8
-descifrar(S, M) :- palabras(S, P), palabras_con_variables(P, V), descifrarPalabras(V, Mvar), juntar_con(Mvar, 32, Palabrasseparadas), string_codes(M, Palabrasseparadas).
+descifrar(S, M) :- palabras(S, P), 	
+				   palabras_con_variables(P, V), 
+				   descifrarPalabras(V, Mvar), 
+				   juntar_con(Mvar, 32, Palabrasseparadas), 
+				   string_codes(M, Palabrasseparadas).
 
 descifrarPalabras([], []).
-descifrarPalabras([Ps | Pss], Mvar) :- diccionario_lista(PalabraDelDicc), descifrarPalabras(Pss, Mrec), palabra_valida(Ps, PalabraDelDicc, Mp), append([Mp], Mrec, Mvar).
+descifrarPalabras([Ps | Pss], Mvar) :- diccionario_lista(PalabraDelDicc), 
+									   descifrarPalabras(Pss, Mrec), 
+									   palabra_valida(Ps, PalabraDelDicc, Mp), 
+									   append([Mp], Mrec, Mvar).
 
 palabra_valida([], [], []).
-palabra_valida([Var | Vars], [P | Ps], M) :- length(Vars, Lv), length(Ps, Lp), Lv == Lp, palabra_valida(Vars, Ps, Mrec), P = Var, append([P], Mrec, M).
+palabra_valida([Var | Vars], [P | Ps], M) :- length(Vars, Lv), 
+											 length(Ps, Lp), 
+											 Lv == Lp, 
+											 palabra_valida(Vars, Ps, Mrec), 
+											 P = Var, 
+											 append([P], Mrec, M).
 
 %Ejercicio 10
-%ESTA MAL LOS TIPOS QUE PASO DESDE EL PRINCIPIO. VER QUE TRAE DESCIFRAR_SIN_ESPACIOS Y A PARTIR DE ESO REHACER! LA IDEA ESTA!
-
-%mensajes_mas_parejos(S, M) :- descifrar_sin_espacios(S, M), calcular_desvio(M, DesvioM), descifrar_sin_espacios(S, MComparador), calcular_desvio(MComparador, DesvioComp), DesvioM =< DesvioComp.
+mensajes_mas_parejos(S, M) :- descifrar_sin_espacios(S, M), 
+							  calcular_desvio(M, DesvioM), 
+							  descifrar_sin_espacios(S, MComparador), 
+							  calcular_desvio(MComparador, DesvioComp), 
+							  DesvioM =< DesvioComp.
  
+calcular_desvio(Mensaje, Desvio) :- split_por_espacio(Mensaje, MsjSeparadosPorEspacio), 
+								    calcular_desvio_sobre_lista_de_palabras(MsjSeparadosPorEspacio, Desvio).
 
-%calcular_desvio(Mensaje, Desvio) :- split_string_by_space(Mensaje, MsjSeparadosPorEspacio), calcular_desvio_sobre_lista_de_palabras(MsjSeparadosPorEspacio, Desvio).
+split_por_espacio([],[]).
+split_por_espacio(Sentencia, ListaDePalabras) :- leer_hasta_espacio(Sentencia, Palabra),
+										   borrar_primera_palabra(Palabra, Sentencia, SSinPalabra),
+										   split_por_espacio(SSinPalabra, RecListaDePalabras),
+										   append([Palabra], RecListaDePalabras, ListaDePalabras), !.
+									
+leer_hasta_espacio(Caracteres, PrimeraPalabra) :- primera_palabra(Caracteres, [], PrimeraPalabra).
 
-%split_string_by_space([letra, Letras], MsjSeparadosPorEspacio) :- append([LLength], MensajeRec, LList), split_string_by_space(Letras, MensajeRec), length(L, LLength),!. 
+primera_palabra([],PrimeraPalabra, PrimeraPalabra).
+primera_palabra([C|Cs],Accum, PrimeraPalabra) :- C\=32,
+									  append(Accum, [C], AccumConCaracter),
+									  primera_palabra(Cs,AccumConCaracter, PrimeraPalabra).
+primera_palabra([C|_],FirstWord,FirstWord) :- C==32.
 
-calcular_desvio_sobre_lista_de_palabras(Palabras, Desvio) :- calcular_longitud_media(Palabras, LongMedia), binomios_cuadrados(Palabras, LongMedia, BCuadrado), sum_list(BCuadrado, Sumatoria), 
-															length(Palabras, LPalabras), Division is Sumatoria / LPalabras, Desvio is sqrt(Division).
+borrar_primera_palabra(Palabra, Sentencia, SentenciaSinPalabra) :- append(Palabra, [32|SentenciaSinPalabra], Sentencia), !.
+borrar_primera_palabra(Palabra, Sentencia, SentenciaSinPalabra) :- append(Palabra, SentenciaSinPalabra, Sentencia).
 
-calcular_longitud_media(P, LongMedia) :- length_list(P, LengthList), average(LengthList, LongMedia).
+calcular_desvio_sobre_lista_de_palabras(Palabras, Desvio) :- calcular_longitud_media(Palabras, LongMedia), 
+															 binomios_cuadrados(Palabras, LongMedia, BCuadrado), 
+															 sum_list(BCuadrado, Sumatoria), 
+															 length(Palabras, LPalabras), Division is Sumatoria / LPalabras, Desvio is sqrt(Division).
 
-length_list([L | Ls], LList) :- append([LLength], LListRec, LList), length_list(Ls, LListRec), length(L, LLength),!. 
+calcular_longitud_media(P, LongMedia) :- length_list(P, LengthList), 
+										 average(LengthList, LongMedia).
+
+length_list([L | Ls], LList) :- append([LLength], LListRec, LList), 
+								length_list(Ls, LListRec),
+								length(L, LLength),!. 
 length_list([L], LList) :- length(L, LLength), LList = [LLength].
 
-average(List, Average):- sum_list(List, Sum), length(List, Length), Length > 0, Average is (Sum / Length).
+average(List, Average):- sum_list(List, Sum), 
+						 length(List, Length), 
+						 Length > 0, 
+						 Average is (Sum / Length).
 
 binomios_cuadrados([P], LongMedia, [BCuadrado]) :- binomio_cuadrado(P, LongMedia, BCuadrado). 
-binomios_cuadrados([P | Ps], LongMedia, BCuadrados) :- append([BCuadrado], RecBCuadrados, BCuadrados), binomios_cuadrados(Ps, LongMedia, RecBCuadrados), binomio_cuadrado(P, LongMedia, BCuadrado),!.
+binomios_cuadrados([P | Ps], LongMedia, BCuadrados) :- append([BCuadrado], 
+													   RecBCuadrados, BCuadrados), 
+													   binomios_cuadrados(Ps, LongMedia, RecBCuadrados), 
+													   binomio_cuadrado(P, LongMedia, BCuadrado),!.
 
-binomio_cuadrado(P, LongMedia, BCuadrado) :- length(P, LongP), Resta is (LongP-LongMedia), BCuadrado is (Resta^2).
+binomio_cuadrado(P, LongMedia, BCuadrado) :- length(P, LongP), 
+											 Resta is (LongP-LongMedia), 
+											 BCuadrado is (Resta^2).
 
-%calcular_desvios([], Desvios) :- Desvios = [].
-%calcular_desvios([Msj | Msjs], Desvios) :- calcular_desvio(Msj, Desvio), calcular_desvios(Msjs, Drec), append(Desvio, Drec, Desvios).  
 
 % listar mensajes secretos de ejemplo.
 ej(1, [rombo, cuadrado, espacio, perro, cuadrado, sol, cuadrado]).
